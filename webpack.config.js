c01f2cdd32ad8fca885748a0834ed29b130e0547
Guillermo Aszyn <guilleaszyn@gmail.com>
2018-01-10 17:28:58 -0300

    correccion webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,15 @@ module.exports = {
   },
   context: __dirname,
   devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
     loaders: [
       {
         test: /jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
